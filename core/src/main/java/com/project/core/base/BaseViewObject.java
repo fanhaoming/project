89edf5f117eac182ad3c452754b4f3b31193d534
fanhaoming <393402303@qq.com>
2018-05-31 18:05:14 +0800

    JSON数据处理

---
+++ core/src/main/java/com/project/core/base/BaseViewObject.java
@@ -0,0 +1,22 @@
+package com.project.core.base;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Created by Administrator on 2018/5/13.
+ */
+@Slf4j
+public class BaseViewObject {
+
+    private String id;
+
+    public String getId() {
+
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
---
+++ core/src/main/java/com/project/core/json/CustomBeanSerializerModifier.java
@@ -0,0 +1,13 @@
+package com.project.core.json;
+
+import com.fasterxml.jackson.databind.ser.BeanSerializerModifier;
+
+/**
+ * @ClassName CustomBeanSerializerModifier
+ * @Description TODO
+ * @Author fanhaoming
+ * @Date 2018/5/31  17:36
+ * @Version 1.0
+ **/
+public class CustomBeanSerializerModifier extends BeanSerializerModifier {
+}
